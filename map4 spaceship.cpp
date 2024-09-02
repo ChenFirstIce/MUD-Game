246171df1cb0c03c9c25515da58a762a9796c29e
cyf8 <2308016738@qq.com>
2024-09-02 14:26:50 +0800

    Add files via upload

---
+++ map4 spaceship.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include"map.h"
+
+void ShowShip() {                                                      
+    cout << "                                      #" << endl;
+    cout << "                                    @ .@ " << endl;
+    cout << "                                   @   & " << endl;
+    cout << "                                  @    @*" << endl;
+    cout << "                                    % @  " << endl;
+    cout << "                                      @ " << endl;
+    cout << "                                       @ " << endl;
+    cout << "                                         @ " << endl;
+    cout << "                                         @ " << endl;
+    cout << "                                          @ " << endl;
+    cout << "                                    #@@@@@@@@ " << endl;
+    cout << "                                @@           #@; " << endl;
+    cout << "                              @                 @@ " << endl;
+    cout << "                             @                    @." << endl;
+    cout << "                           @                        @ " << endl;
+    cout << "                          #  @. @                    @ " << endl;
+    cout << "                          ;  ;   %   ;                @ " << endl;
+    cout << "                         #       $  !  #               @" << endl;
+    cout << "                   !$#@@@    @  @  @         #  " << endl;
+    cout << "          *@@@@$        !@        @        %  @    @@$ @ " << endl;
+    cout << "       @@.                 @@      @  @   @       @   *       " << endl;
+    cout << "      &@*       @o@@         @@          @       @   @ @      " << endl;
+    cout << "        @ @       @@%@           @@.       @ .@   @   @ @     " << endl;
+    cout << "         @ $@                       #@@            @@@  ;" << endl;
+    cout << "           %  %@           $@@@          &@@!" << endl;
+    cout << "           $o    @@         @@ @              .@@@@.      &" << endl;
+    cout << "            @      #@                                !#@@@@" << endl;
+    cout << "            @   @@              @@@&                 @" << endl;
+    cout << "          %@@           @@  *                 !@" << endl;
+    cout << "        @@@@         *@@                 #@@@           @@" << endl;
+    cout << "       $&    @            #@@              @@o@      @@@@  #*" << endl;
+    cout << "       !     @@                @@@                    #@;o#   @" << endl;
+    cout << "       @   @   @                   @@@!                        @$" << endl;
+    cout << "     %  .o     @                      ;@@@&                     @" << endl;
+    cout << "     @  @        @                           @@@@$               @" << endl;
+    cout << "    @@  #         @@                               *@@@@@%        @;" << endl;
+    cout << "   !@  @@@           @                                       &@@@@@@@@" << endl;
+    cout << "  @.      &            @.                                          @@" << endl;
+    cout << "   #@     @              @@                                    o@@" << endl;
+    cout << "    .@@@@@@               &@&@                         @@* #@@" << endl;
+    cout << "                               %                          @" << endl;
+    cout << "                               @  @ #@@@%           &@@@@ @   @" << endl;
+    cout << "                              @  @        .%###*.@##     @  o" << endl;
+    cout << "                            @  @                         @  @" << endl;
+    cout << "                           @   @                         @  @" << endl;
+    cout << "                          @  @                          @  @" << endl;
+    cout << "                          @  @                          @  @" << endl;
+    cout << "                           @  @                          @  @" << endl;
+    cout << "                          @ @@@                        @@ @@" << endl;
+    cout << "                        #&    @                       @      @" << endl;
+    cout << "                       @       @            .         .@     @" << endl;
+    cout << "                        o@     @                        o@@@ @" << endl;
+    cout << "                          %@@@@@" << endl;
+                                                                                                                                                      
+
+}
